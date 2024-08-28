89d5b6ed885460d5306c46040ee2fb92bbbf4e92
Turan Heydarli <turan.heydarli@code.edu.az>
2024-08-28 14:56:23 +0400

    progect base initialized

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,3 +1,27 @@
-module.exports = {
-    
-}
+const merge = require("webpack-merge");
+const path = require("path");
+const base = require("./base");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge.merge(base, {
+  mode: "production",
+  output: {
+    filename: "bundle.min.js"
+  },
+  devtool: false,
+  performance: {
+    maxEntrypointSize: 900000,
+    maxAssetSize: 900000
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  }
+});
